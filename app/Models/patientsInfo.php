e4f4da749bf5fad581e3f688e562a2c57a0b2602
Hisukawa <hisukawataiki123@gmail.com>
2026-04-03 21:22:50 +0800

    encyption removed

--- app/Models/patientsInfo.php
+++ app/Models/patientsInfo.php
@@ -7,22 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class patientsInfo extends Model
 {
     protected $table = 'patients_info';
-    
+
     protected $fillable = [
         'patient_id', 'sex', 'civil_status', 'nationality', 'birthdate', 'place_of_birth', 'religion', 'phone_number'
     ];
 
-    protected function casts(): array
-    {
-        return [
-            'civil_status' => 'encrypted',
-            'nationality' => 'encrypted',
-            'birthdate' => 'encrypted',
-            'place_of_birth' => 'encrypted',
-            'religion' => 'encrypted',
-            'phone_number' => 'encrypted',
-        ]; 
-    }
+    // protected function casts(): array
+    // {
+    //     return [
+    //         'civil_status' => 'encrypted',
+    //         'nationality' => 'encrypted',
+    //         'birthdate' => 'encrypted',
+    //         'place_of_birth' => 'encrypted',
+    //         'religion' => 'encrypted',
+    //         'phone_number' => 'encrypted',
+    //     ];
+    // }
 
     public function address() {
         return $this->hasOne(patientsAddress::class, 'patient_info_id');
